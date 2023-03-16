1ca375797c9751648a79c71cb5f7d567c5cbd060
marufbroh <marufmd775@gmail.com>
2023-03-16 12:09:58 +0600

    blog html added

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    // console.log(quiz)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -110,7 +109,6 @@ document.querySelector("#submit").addEventListener("click", () => {
 
   // data setting on local storage and getting data from local storage
   let storage = JSON.parse(localStorage.getItem("results"));
-  console.log(storage)
   if (storage) {
     localStorage.setItem(
       "results",
